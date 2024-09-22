590a834c2bbd9abd763f1aed18b03bfb724e82d0
tetyanadixon <tetyanadixon@gmail.com>
2024-09-22 06:32:42 -0500

    adds function for menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi, welcome to my portfolio wesite!")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
